5070576191e962efce5694215264dc09ae4bee40
Michel Albert <michel@albert.lu>
2017-01-20 10:36:06 +0100

    CRON subcommand added.
    Passing script-path is also no longer required.

--- munininfluxdb/commands/cron.py
+++ munininfluxdb/commands/cron.py
@@ -19,7 +19,7 @@ NAME = 'cron'
 DESCRIPTION = 'Installs or uninstalls the CRON job'
 
 
-def uninstall_cron():
+def uninstall_cron(args):
     if os.geteuid() != 0:
         print("It seems you are not root, please run \"muninflux fetch --uninstall-cron\" again with root privileges")
         sys.exit(1)
@@ -34,12 +34,20 @@ def uninstall_cron():
     cron.remove(*jobs)
     cron.write()
 
-    return len(jobs)
+    nb = len(jobs)
+    if nb:
+        print("{0} Cron job uninstalled for user {1} ({2} entries deleted)".format(Symbol.OK_GREEN, CRON_USER, nb))
+    else:
+        print("No matching job found (searching comment \"{1}\" in crontab for user {2})".format(Symbol.WARN_YELLOW,
+                                                                                                 CRON_COMMENT, CRON_USER))
 
 
-def install_cron(script_file, period):
+def install_cron(args):
+    script_path = absolute_executable()
+    cmd = '%s fetch' % script_path
+
     if os.geteuid() != 0:
-        print("It seems you are not root, please run \"muninflux fetch --install-cron\" again with root privileges")
+        print("It seems you are not root, please run \"%s cron install\" again with root privileges")
         sys.exit(1)
 
     try:
@@ -48,41 +56,25 @@ def install_cron(script_file, period):
         from vendor import crontab
 
     cron = crontab.CronTab(user=CRON_USER)
-    job = cron.new(command=script_file, user=CRON_USER, comment=CRON_COMMENT)
-    job.minute.every(period)
+    job = cron.new(command=cmd, user=CRON_USER, comment=CRON_COMMENT)
+    job.minute.every(args.period)
 
     if job.is_valid() and job.is_enabled():
         cron.write()
 
+    print("{0} Cron job installed for user {1}".format(Symbol.OK_GREEN, CRON_USER))
     return job.is_valid() and job.is_enabled()
 
 
 def setup(parser):
-    parser.add_argument('script_path',
-                        help='install a cron job to updated InfluxDB with fresh data from Munin every <period> minutes')
-    parser.add_argument('-p', '--period', default=5, type=int,
-                        help="sets the period in minutes between each fetch in the cron job (default: %(default)dmin)")
-    parser.add_argument('--uninstall-cron', action='store_true',
-                        help='uninstall the fetch cron job (any matching the initial comment actually)')
-    parser.set_defaults(func=main)
+    subparsers = parser.add_subparsers(title='CRON commands')
+    install_parser = subparsers.add_parser(
+        'install', description='Installs the CRON job')
+    uninstall_parser = subparsers.add_parser(
+        'uninstall', description='Uninstalls the CRON job')
 
-
-def main(args):
-    print(absolute_executable())
-    raise NotImplementedError('Not yet implemented as subcommand')
-    # TODO from pkg_resources import load_entry_point
-    # TODO entry = load_entry_point('munin-influxdb', 'console_scripts', 'muninflux_fetch')
-    # TODO # python bin/fetch.py --install-cron $(dirname $(readlink -f "$0"))/bin/fetch.py
-
-    if args.script_path:
-        install_cron(args.script_path, args.period)
-        print("{0} Cron job installed for user {1}".format(Symbol.OK_GREEN, CRON_USER))
-        return
-    elif args.uninstall_cron:
-        nb = uninstall_cron()
-        if nb:
-            print("{0} Cron job uninstalled for user {1} ({2} entries deleted)".format(Symbol.OK_GREEN, CRON_USER, nb))
-        else:
-            print("No matching job found (searching comment \"{1}\" in crontab for user {2})".format(Symbol.WARN_YELLOW,
-                                                                                                     CRON_COMMENT, CRON_USER))
-        return
+    install_parser.add_argument(
+        '-p', '--period', default=5, type=int,
+        help="sets the period in minutes between each fetch in the cron job (default: %(default)dmin)")
+    install_parser.set_defaults(func=install_cron)
+    uninstall_parser.set_defaults(func=uninstall_cron)
