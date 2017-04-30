7bce6455697885043b03c5992df8d4461f97fa8e
Michel Albert <michel@albert.lu>
2017-04-30 12:43:30 +0200

    Prepare main module for unit-testing.

--- munininfluxdb/main.py
+++ munininfluxdb/main.py
@@ -10,11 +10,40 @@ import munininfluxdb.commands.import_ as cmd_import
 import munininfluxdb.external.cron as cron
 
 
-def main():
+def dummy_function(args):
+    """
+    Dummy function which is called if no subcommand was loaded.
+
+
+    Developer Note:
+
+        This exists mainly that so that we can mock it during unit-tests and
+        check that the submodule function is called properly without actually
+        calling any subcommand.
+    """
+    print('Dummy function. If you see this, no subcommand was loaded. '
+          'It is HIGHLY unlikely that you see this! '
+          'If you do, please contact the developers!')
+    return 0
+
+
+def main(args=None, commands=None):
+    # Allow some unit-tetst injections
+    args = args or sys.argv[1:]
+    if not commands:
+        commands = {
+            'cron': cmd_cron,
+            'dump': cmd_dump,
+            'fetch': cmd_fetch,
+            'import': cmd_import,
+        }
+
+    # Prepare the CLI parser
     parser = ArgumentParser(description='TODO')  # TODO
     parser.add_argument('--interactive', dest='interactive', action='store_true')
     parser.add_argument('--no-interactive', dest='interactive', action='store_false')
     parser.set_defaults(interactive=True)
+    parser.set_defaults(func=dummy_function)
 
     subparsers = parser.add_subparsers(
         title='subcommands',
@@ -22,20 +51,23 @@ def main():
         help='additional help'
     )
 
-    for subcommand in (cmd_import, cmd_fetch, cmd_cron, cmd_dump):
+    # Initialise CLI argument parser for the subcommands
+    for subcommand in commands.values():
         subparser = subparsers.add_parser(subcommand.NAME,
                                           description=subcommand.DESCRIPTION)
         subcommand.setup(subparser, {
             'cron': cron
         })
 
-    args = parser.parse_args()
+    # Parse the arguments and execute the command
+    namespace = parser.parse_args(args)
     try:
-        args.func(args)
+        namespace.func(namespace)
     except KeyboardInterrupt:
         print("\n{0} Canceled.".format(Symbol.NOK_RED))
-        sys.exit(1)
+        return 1
     except Exception as e:
         raise
         print("{0} Error: {1}".format(Symbol.NOK_RED, e))
-        sys.exit(1)
+        return 1
+    return 0
